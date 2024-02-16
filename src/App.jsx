19b557ec21f6bf92a2b9c1a97ddf2a37c29294ee
Gabriel15066 <gabriel.mchiri@epitech.eu>
2024-02-16 22:26:21 +0100

    Just styling the game with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-//notions:
-//Passage de valeurs entre composants à l'aide de 'props', qui sont les arguments de composants
-//A state is private to a component that defines it,
-//setter permettent de dire a react que le state d'un composant a change et donc le reafficher
-// props Square:
-// {null} value
-// {event} onSquareClick   //les deux sont des props
-// i : index de la case
-// la fonction fléchee (fonction() => ) évite les boucles infinies et va associer une fonction a notre evenement et evite de créer une fonction pour chaque index
-// toutes les données sont gérées par Board
-//par convention on met en argument (props) les evenements avec onSomething et les fonctions
-// qui gèrent ces evenements avec handleSomething
-// pour gérer les tours, on attribue une valeur true au X qui va changer alternativement a
-// chaque fois qu'on va clicker et donc changer la valeur a 'O'
 
 
 function Square({value, onSquareClick}) {
     return (
-    <button
-    className="square"
-    onClick={onSquareClick}
-    >{value}
+        <button
+        className="square"
+        onClick={onSquareClick}
+        >{value}
     </button>
-    );
-}
-
-function Board() {
-    const [xIsNext, setXIsNext] = useState(true)
-    const [squares, setSquares] = useState(Array(9).fill(null))
-    const winner = calculateWinner(squares)
-    let status
-    function handleClick(i) {    //slice() permet de créer une copie de notre tableau squares pour ne pas modifier directement ces valeurs.
-        if (squares[i] || calculateWinner(squares))
-            return;
-        const nextSquares = squares.slice();        //nextSquares est juste une copie de notre tableau squares.
-        if (xIsNext)
-            nextSquares[i] = 'X'
-        else
-            nextSquares[i] = 'O'
-        setSquares(nextSquares)
-        setXIsNext(!xIsNext)
-}
-
-    if (winner) {
-        status = "Winner" + winner;
-    } else {
-        status = "Next player :" + (xIsNext ? "X" : "O");
-    }
-
-return (
-    <div content='center'>
-        <div className='status'>{status}</div>
-        <div className="board-row">
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-        </div>
-        <div className="board-row">
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-        </div>
-        <div className="board-row">
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-        </div>
-    </div>
     );
 }
 
@@ -90,4 +31,64 @@ function calculateWinner(squares) {
     }
     return null;
 }
-export default Board;
+
+function Board() {
+    const [xIsNext, setXIsNext] = useState(true)
+    const [squares, setSquares] = useState(Array(9).fill(null))
+    const winner = calculateWinner(squares)
+    let status
+
+    function handleClick(i) {    //slice() permet de créer une copie de notre tableau squares pour ne pas modifier directement ces valeurs.
+        if (squares[i] || calculateWinner(squares))
+        return;
+    const nextSquares = squares.slice();        //nextSquares est juste une copie de notre tableau squares.
+        if (xIsNext)
+        nextSquares[i] = 'X'
+        else
+        nextSquares[i] = 'O'
+        setSquares(nextSquares)
+        setXIsNext(!xIsNext)
+    }
+
+    if (winner) {
+        status = "Winner:   " + winner;
+    } else {
+        status = "Next player :  " + (xIsNext ? "X" : "O");
+    }
+
+    return (
+        <div className='complete-board'>
+        <div className='status'>{status}</div>
+        <div className='nine_squares'>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+function Game() {
+
+    return <div className='game'>
+        <div className='game-board'>
+            <Board />
+        </div>
+        <div className='game-info'>
+        </div>
+    </div>
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Square from './App';
-import Board from './App';
+import Game from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Board />
+    <Game />
   </React.StrictMode>
 );
 
